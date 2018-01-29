4699d08633fc0d78e36d51ac022e029c3d65a303
Aleksandr <duxard@gmail.com>
2018-01-29 14:51:41 +0300

    minor nav change

--- src/app/components/Navigation.js
+++ src/app/components/Navigation.js
@@ -17,19 +17,9 @@ export default class Navigation extends React.Component {
                         </div>
                         <div className="collapse navbar-collapse" id="responsiveMenu">
                             <ul className="nav navbar-nav">
-                                <li><a href="#">Item 1</a></li>
-                                <li className="dropdown">
-                                    <a href="#" className="dropdown-toggle" data-toggle="dropdown">Item 2<b className="caret"></b></a>
-                                    <ul className="dropdown-menu">
-                                        <li><a href="#">Subitem 1</a></li>
-                                        <li><a href="#">Subitem 2</a></li>
-                                        <li><a href="#">Subitem 3</a></li>
-                                        <li className="divider"></li>
-                                        <li><a href="#">Subitem 4</a></li>
-                                    </ul>
-                                </li>
-                                <li><a href="#">Item 3</a></li>
-                                <li><a href="#">Item 4</a></li>
+                                <li><a href="#">Words</a></li>
+                                <li><a href="#">Links</a></li>
+                                <li><a href="#">Info</a></li>
                             </ul>
                         </div>
                     </div>
